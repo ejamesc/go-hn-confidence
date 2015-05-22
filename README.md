1b2d0bdd20bc58424f846e7dac6b3e8324553bf7
Cedric Chin <cedric@elijames.org>
2015-05-22 23:29:29 +0700

    Initial commit.

---
+++ README.md
@@ -0,0 +1,11 @@
+# Go Hacker News Confidence
+
+This project grabs Hacker News, sorts them according to the lower bound of a Wilson score confidence interval for a Bernoulli parameter, and spits it out as HTML. We assume points as upvotes and comments as downvotes. 
+
+## Slightly longer explanation
+This project is how I weaned myself off Hacker News. I've found that the most
+worthwhile HN articles to read are ones with high upvote to comment ratio.
+Controversial articles tend waste time e.g. "Why Rails sucks"; high-quality analysis and
+world-changing news generates little conversation but much upvoting.
+
+This saves everyone some time.
---
+++ hnconf.go
@@ -0,0 +1,106 @@
+package main
+
+import (
+	"fmt"
+	"log"
+	"math"
+	"sort"
+	"strconv"
+	"strings"
+
+	"github.com/PuerkitoBio/goquery"
+)
+
+const ROOT_URL string = "https://news.ycombinator.com"
+
+type NewsItem struct {
+	Title    string
+	Link     string
+	Points   int
+	Comments int
+}
+
+func (ni *NewsItem) Score() float64 {
+	n := float64(ni.Points + ni.Comments)
+	pos := float64(ni.Points)
+	if n == 0 {
+		return 0
+	}
+
+	z := 1.96 // we assume a 95% confidence interval
+	phat := 1.0 * pos / n
+	res := (phat + z*z/(2.0*n) - z*math.Sqrt((phat*(1.0-phat)+z*z/(4.0*n))/n)) / (1.0 + z*z/n)
+	return res
+}
+
+type Items []*NewsItem
+
+func (m Items) Len() int {
+	return len(m)
+}
+
+func (m Items) Swap(i, j int) {
+	m[i], m[j] = m[j], m[i]
+}
+
+func (m Items) Less(i, j int) bool {
+	return m[i].Score() < m[j].Score()
+}
+
+func main() {
+	doc, err := goquery.NewDocument(ROOT_URL)
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	res := []*NewsItem{}
+
+	doc.Find(".athing").Each(func(i int, s *goquery.Selection) {
+		title := s.Find(".title a").Text()
+		link, exists := s.Find(".title a").Attr("href")
+		if !exists {
+			link = ""
+		}
+
+		if strings.HasPrefix(link, "item?") {
+			link = ROOT_URL + "/" + link
+		}
+		item := &NewsItem{Title: title, Link: link}
+		res = append(res, item)
+		//fmt.Printf("%v - %v\n", title, link)
+	})
+
+	doc.Find(".subtext").Each(func(i int, s *goquery.Selection) {
+		pString := s.Find(".score").Text()
+		cString := s.Find("a").Last().Text()
+		points := 0
+		comments := 0
+
+		if pString != "" {
+			pSt := strings.Split(pString, " ")[0]
+			points, err = strconv.Atoi(pSt)
+			fmt.Println(points)
+			if err != nil {
+				points = 0
+			}
+		}
+
+		if cString != "" && cString != "discuss" {
+			cSt := strings.Split(cString, " ")[0]
+			comments, err = strconv.Atoi(cSt)
+			fmt.Println(comments)
+			if err != nil {
+				comments = 0
+			}
+		}
+
+		item := res[i]
+		item.Points = points
+		item.Comments = comments
+	})
+
+	sort.Sort(sort.Reverse(Items(res)))
+	for _, item := range res {
+		fmt.Printf("[%v] %v : %v - %v | %v\n", item.Score(), item.Points, item.Comments, item.Title, item.Link)
+	}
+}
